4b9eba043610a0ef7d7c2ce743bc252225ebfcbe
nguyen_ngoc <nguyenvanngoc1996@gmail.com>
2023-04-12 02:40:26 +0700

    Share movie

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -50,7 +50,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username password password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_144303) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_183237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.string "description", null: false
+    t.string "url", null: false
+    t.string "thumbnail_url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_144303) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "movies", "users"
 end
